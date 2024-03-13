43d9de425a4a172239097b8285fadf7f39352aff
ridwan414-hub <asifmahmoud414@gmail.com>
2024-03-13 19:14:42 +0600

    lint not added

--- part3/models/person.js
+++ part3/models/person.js
@@ -1,45 +1,42 @@
-/* eslint-disable @stylistic/js/linebreak-style */
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
-// eslint-disable-next-line @stylistic/js/linebreak-style
 
 const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-  // eslint-disable-next-line no-unused-vars
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch(error => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
 
 const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minLength: 3,
-    required: true
-  },
-  number: {
-    type: String,
-    minLength: 8,
-    validate: {
-      validator: function (v) {
-        return /\d{2,3}-\d{5,15}/.test(v)
-      },
-      message:props => `${props.value} is not a valid phone number!`
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
     },
-    required:true
-  }
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function (v) {
+                return /\d{2,3}-\d{5,15}/.test(v);
+            },
+            message:props=>`${props.value} is not a valid phone number!`
+        },
+        required:true
+    }
 })
 
 personSchema.set('toJSON', {
-  transform: (document, returnObject) => {
-    returnObject.id = returnObject._id.toString()
-    delete returnObject._id
-    delete returnObject.__v
-  }
+    transform: (document, returnObject) => {
+        returnObject.id = returnObject._id.toString()
+        delete returnObject._id
+        delete returnObject.__v
+    }
 })
 
 module.exports = mongoose.model('Person',personSchema)
--- part3/mongo.js
+++ part3/mongo.js
@@ -1,11 +1,9 @@
-/* eslint-disable @stylistic/js/linebreak-style */
-// eslint-disable-next-line no-unused-vars
 const { response } = require('express')
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('Please provide passwod as argument')
-  process.exit(1)
+    console.log('Please provide passwod as argument')
+    process.exit(1)
 }
 
 const password = process.argv[2]
@@ -13,27 +11,27 @@ const url = `mongodb+srv://fullstack:${password}@cluster0.uddmrpu.mongodb.net/ph
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+    name: String,
+    number: String
 })
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
-  Person.find({}).then(response => {
-    console.log('PhoneBook: ')
-    response.forEach(person => console.log(`${person.name} ${person.number}`))
-    mongoose.connection.close()
-  })
+    Person.find({}).then(response => {
+        console.log('PhoneBook: ')
+        response.forEach(person => console.log(`${person.name} ${person.number}`));
+        mongoose.connection.close()
+    })
 }
 else {
-  const person = new Person(
-    {
-      name: 'Arto Hellas',
-      number: '040-123456'
+    const person = new Person(
+        {
+            name: "Arto Hellas",
+            number: "040-123456"
+        })
+    
+    person.save().then(result => {
+        console.log(`${result.name} ${result.number} saved successfully!`)
+        mongoose.connection.close()
     })
-
-  person.save().then(result => {
-    console.log(`${result.name} ${result.number} saved successfully!`)
-    mongoose.connection.close()
-  })
 }
